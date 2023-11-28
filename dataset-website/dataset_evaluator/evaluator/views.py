33cfa4e44eed0fae076efecd0717ec0586bbca54
Mohammadreza Mofayezi <mofayezi.m@gmail.com>
2023-11-29 00:21:13 +0330

    Adding dynamic grid size

--- dataset-website/dataset_evaluator/evaluator/views.py
+++ dataset-website/dataset_evaluator/evaluator/views.py
@@ -16,7 +16,8 @@ def start_page(request):
     # Get a list of image files in the folder
     global image_files
     if not image_files:
-        image_files = [f for f in os.listdir(settings.IMAGE_FOLDER) if f.endswith('.jpg')]
+        list_dir = sorted(os.listdir(settings.IMAGE_FOLDER))
+        image_files = [f for f in list_dir if f.endswith('.jpg')]
     all_batches = range(1, len(image_files) // settings.BATCH_SIZE + 2)  # Assuming you have batches from 1 to 100
     # all_batches = range(1, 404)  # Assuming you have batches from 1 to 100
 
@@ -28,6 +29,7 @@ def start_page(request):
 
 def image_grid(request):
     batch_number = int(request.GET.get('batch_number'))
+    column_number = int(request.GET.get('column_number'))
     # # Define the path to your images folder
     # images_folder = 'static/images'  # Adjust the folder path as needed
 
@@ -46,7 +48,10 @@ def image_grid(request):
     # Generate a list of dictionaries with image information
     images = [{'id': os.path.splitext(image)[0], 'file_path': os.path.join(settings.IMAGE_FOLDER, image)} for image in batch_images]
 
-    return render(request, 'image_grid.html', {'batch_number': batch_number, 'images': images, 'is_last_batch': is_last_batch})
+    return render(request, 'image_grid.html', {'batch_number': batch_number, 
+                                               'column_number': column_number, 
+                                               'images': images, 
+                                               'is_last_batch': is_last_batch})
 
 @csrf_exempt
 def save_selected_images(request):
